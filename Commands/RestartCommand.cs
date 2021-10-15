76a3551b8d9fbda24e3f5ede88a3631e90135f9d
tyler staples <tyler.a.staples@gmail.com>
2021-10-15 15:48:39 -0700

    Add restart command

---
+++ Commands/RestartCommand.cs
@@ -0,0 +1,23 @@
+﻿using WinstonBot.Attributes;
+
+namespace WinstonBot.Commands
+{
+    [Command("restart", "Restart the bot", DefaultPermission.AdminOnly)]
+    internal class RestartCommand : CommandBase
+    {
+        public override async Task HandleCommand(CommandContext context)
+        {
+            await context.RespondAsync("Restarting right meow");
+
+            Console.WriteLine("Stopping client...");
+            await context.Client.StopAsync();
+
+            context.Client.Disconnected += (args) =>
+            {
+                Console.WriteLine("Exiting!");
+                Environment.Exit(1);
+                return Task.CompletedTask;
+            };
+        }
+    }
+}
