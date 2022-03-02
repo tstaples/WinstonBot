3be3405505ef0916fa9547ad9bfcac6f9cdc57b1
Phillip Smith <feerip@gmail.com>
2022-03-02 10:15:33 -0800

    don't mind this commit.

--- Commands/SayCommand.cs
+++ Commands/SayCommand.cs
@@ -19,4 +19,18 @@ namespace WinstonBot.Commands
             await context.SendMessageAsync(Message, allowedMentions: new AllowedMentions(AllowedMentionTypes.Roles | AllowedMentionTypes.Users));
         }
     }
+
+    [Command("meow", "Annoy Catman", DefaultPermission.Everyone)]
+    [ScheduableCommand]
+    [ConfigurableCommand]
+    public class meow : CommandBase
+    {
+        public meow(ILogger logger) : base(logger) { }
+
+        public async override Task HandleCommand(CommandContext context)
+        {
+            await context.SendMessageAsync($"<@141439679890325504>", allowedMentions: AllowedMentions.All);
+            await context.SendMessageAsync($"<a:MEOW:881462772636995595>");
+        }
+    }
 }
